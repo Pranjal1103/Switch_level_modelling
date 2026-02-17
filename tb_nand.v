17012ee6498ac8e7691a674d3dfdf5bd51db4a0a
Pranjal Kalal <pranjalrkalal29@gmail.com>
2026-02-17 21:00:58 +0530

    tb_nand.v

---
+++ tb_nand.v
@@ -0,0 +1,17 @@
+module tb_nand;
+  reg a,b;
+  wire out,s1;
+  nand_gate m1(.a(a),.b(b),.out(out));
+  initial begin
+    $dumpfile("dump.vcd"); $dumpvars;
+    a=0;b=0;
+    #10;
+    a=0;b=1;
+    #10;
+    a=1;b=0;
+    #10;
+    a=1;b=1;
+    #10;
+    
+  end
+endmodule
